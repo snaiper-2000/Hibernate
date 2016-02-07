cc051dac0d7688b72e662f5ea0f8955dbd28b727
Dimka <snaiper-2000@mail.ru>
2016-02-07 21:58:33 +0300

    test Hibernate

---
+++ Maven/src/main/java/Hibernate/Main.java
@@ -0,0 +1,9 @@
+package Hibernate;
+
+import org.hibernate.SessionFactory;
+
+public class Main {
+	public static void main(String[] args) {
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+	}
+}
